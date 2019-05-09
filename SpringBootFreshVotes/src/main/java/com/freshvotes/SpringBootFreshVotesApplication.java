7e573e33a999254d631ebe7c7b053e236c1354ef
rabin153 <rabinkarki153@gmail.com>
2019-05-09 12:23:12 +0545

    first commit

---
+++ SpringBootFreshVotes/src/main/java/com/freshvotes/SpringBootFreshVotesApplication.java
@@ -0,0 +1,13 @@
+package com.freshvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreshVotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreshVotesApplication.class, args);
+	}
+
+}
